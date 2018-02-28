e5aa12ab1699278c3c8c91dad1345530f1a0e33b
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2018-02-28 15:30:02 +0100

    tweak phonescreen

--- phonescreen.cpp
+++ phonescreen.cpp
@@ -17,7 +17,7 @@ PhoneScreen::PhoneScreen(QWidget *parent) :
     connect(ui->button8, &QPushButton::clicked, this, [this]{ nr(8); });
     connect(ui->button9, &QPushButton::clicked, this, [this]{ nr(9); });
     connect(ui->buttonDel, &QPushButton::clicked, this, &PhoneScreen::delNr);
-//    connect(ui->buttonOk, &QPushButton::clicked, this, %Dial::ok);
+    connect(ui->buttonHome, &QPushButton::clicked, this, &PhoneScreen::gotoHomeScreen);
 //    connect(ui->buttonAvbryt, &QPushButton::clicked, this, %Dial::avbryt);
 }
 
@@ -28,10 +28,10 @@ PhoneScreen::~PhoneScreen()
 
 void PhoneScreen::nr(int digit) {
     number += QString::number(digit);
-    ui->label->setText(tr("Number: %0").arg(number));
+    ui->label->setText(number);
 }
 
 void PhoneScreen::delNr() {
     number.chop(1);
-    ui->label->setText(tr("Number: %0").arg(number));
+    ui->label->setText(number);
 }
