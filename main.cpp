9902831b4c7655346e3ced2e41e5fee6bcf14666
Daniel Marjamäki <daniel.marjamaki@gmail.com>
2018-03-03 14:20:45 +0100

    attempt to show the mainwindow as maximized and without titlebar

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    w.showMaximized();
 
     gsmInterface.restart();
 
